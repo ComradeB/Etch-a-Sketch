b90f8f30e01ec8ae673eb18da4a78eba8ed3b20d
Bijan <bijansamie262@mail.ru>
2021-11-14 01:12:42 +0300

    centered elements

--- script.js
+++ script.js
@@ -3,20 +3,12 @@ const gridContainer = document.querySelector('.grid-container')
 const resetButton = document.querySelector('.reset')
 const rainbowButton = document.querySelector('.rainbow-mode')
 
-
-// create default grid
-// mouseover along grid
-
-// enable/disable rainbow mode
-
-// reset grid: clear. prompt side length, recreate grid
-
 function createDefaultGrid(sideLength) {
     
     for (let i = 0; i < sideLength**2; i++) {
         const gridItem = document.createElement('div')
         gridItem.addEventListener('mouseover', () => {
-            gridItem.classList.add('fill-black')
+            gridItem.style.backgroundColor = 'black'
         });
         rainbowButton.addEventListener('click', () => {
             gridItem.addEventListener('mouseover', () => {
